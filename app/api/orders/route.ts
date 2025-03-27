ecaf74804302795c5da5026631f0ef9aa2d0896d
Purnima <puri143@yahoo.com>
2025-03-27 10:51:35 +0530

    authoption fix

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -2,7 +2,7 @@ import { connectToDatabase } from "@/lib/mongodb";
 import Order from "@/models/Order";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"; // ✅ Ensure NextAuth is configured
+import { authOptions } from "@/lib/auth"; // ✅ Ensure NextAuth is configured
 
 // ✅ POST: Save order to MongoDB
 export async function POST(req: Request) {
