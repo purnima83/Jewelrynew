60641a448196b1592ae7aab0177bf52b32f0ce0c
Purnima <puri143@yahoo.com>
2025-04-22 16:06:21 +0530

    google redirect

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,17 +1,26 @@
 import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      email: string;
-      role?: string;
+      email?: string | null;
+      name?: string | null;
+      image?: string | null;
+      role?: string;  
     };
   }
-
   interface User {
     id: string;
     email: string;
+    role?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
     role?: string;
   }
 }
