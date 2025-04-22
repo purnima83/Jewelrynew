d2ba208924e72da2bd994b2199fa2bda9eff3c5f
Purnima <puri143@yahoo.com>
2025-04-22 20:02:56 +0530

    google redirect

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,4 @@
-// @ts-nocheck
-
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
-
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
